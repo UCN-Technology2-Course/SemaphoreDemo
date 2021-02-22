5df448ad481aa265c193bb9b28f9a5ccca704789
Lars Nysom <LANY@ucn.dk>
2021-02-22 12:34:12 +0100

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.Semaphore;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Semaphore semaphore = new Semaphore(4);
+		
+		System.out.println("Total available Semaphore permits : " 
+                + semaphore.availablePermits());
+    
+        Atm t1 = new Atm("A", semaphore);
+        t1.start();
+
+        Atm t2 = new Atm("B", semaphore);
+        t2.start();
+
+        Atm t3 = new Atm("C", semaphore);
+        t3.start();
+
+        Atm t4 = new Atm("D", semaphore);
+        t4.start();
+
+        Atm t5 = new Atm("E", semaphore);
+        t5.start();
+
+        Atm t6 = new Atm("F", semaphore);
+        t6.start();
+
+	}
+
+}
