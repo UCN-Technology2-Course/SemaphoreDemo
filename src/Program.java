70cc9020d4f2820f6019d9006450e1cb77fe44f0
Lars Nysom <LANY@ucn.dk>
2021-04-12 15:39:42 +0200

    Simplyfied the Program class

--- src/Program.java
+++ src/Program.java
@@ -8,24 +8,29 @@ public class Program {
 		
 		System.out.println("Total available Semaphore permits : " 
                 + semaphore.availablePermits());
-    
-        Atm t1 = new Atm("A", semaphore);
-        t1.start();
-
-        Atm t2 = new Atm("B", semaphore);
-        t2.start();
-
-        Atm t3 = new Atm("C", semaphore);
-        t3.start();
-
-        Atm t4 = new Atm("D", semaphore);
-        t4.start();
-
-        Atm t5 = new Atm("E", semaphore);
-        t5.start();
-
-        Atm t6 = new Atm("F", semaphore);
-        t6.start();
+		
+//        Atm t1 = new Atm("A", semaphore);
+//        t1.start();
+//
+//        Atm t2 = new Atm("B", semaphore);
+//        t2.start();
+//
+//        Atm t3 = new Atm("C", semaphore);
+//        t3.start();
+//
+//        Atm t4 = new Atm("D", semaphore);
+//        t4.start();
+//
+//        Atm t5 = new Atm("E", semaphore);
+//        t5.start();
+//
+//        Atm t6 = new Atm("F", semaphore);
+//        t6.start();
+		
+		// I stedet for at lave en variabel til hver atm, kan de også startes direkte som vist herunder.
+		for(int i = 1; i <= 6; i++) {
+			new Atm("ATM "+ i, semaphore).start();
+		}
 
 	}
 
